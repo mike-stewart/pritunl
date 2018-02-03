00e19f1bbd65df7e0f3b11811cb981bf6a88292d
Mike Stewart <mike.stewart@introhive.com>
2018-02-03 19:15:54 -0400

    Remove unused resource definitions.

--- recipes/_common.rb
+++
@@ -1,2 +0,0 @@
-
-chef_gem "rest-client"
--- resources/organization.rb
+++
@@ -1,8 +0,0 @@
-actions :create, :delete
-default_action :create
-
-attribute :name, :kind_of => String, :name_attribute => true
-
-attribute :response, :kind_of => String
-
-attr_accessor :exists
